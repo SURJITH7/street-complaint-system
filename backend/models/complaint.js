56ecf4b536889cd032c672bff574b957c2108d8b
SURJITH7 <surjithrock3548@gmail.com>
2026-05-20 21:02:46 +0530

    Completed MERN backend setup and MongoDB connection

---
+++ backend/models/complaint.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const complaintSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    category: {
+        type: String,
+        required: true
+    },
+
+    location: {
+        type: String,
+        required: true
+    },
+
+    status: {
+        type: String,
+        default: 'Pending'
+    }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('Complaint', complaintSchema);
